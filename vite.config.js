408a17fb77255e28736636a5db2a07747a429b21
SWE | Hassan <148480926+HsnBGit@users.noreply.github.com>
2024-10-31 17:28:01 +0300

    Update vite.config.js to change the res name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-v5/", // YOUR REPO NAME HERE
+  base: "/Portfolio/", // YOUR REPO NAME HERE
   plugins: [react()],
 })
